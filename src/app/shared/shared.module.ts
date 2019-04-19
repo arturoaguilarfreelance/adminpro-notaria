563f931d0cccbb1a55183a2458be10cf6d1b3714
Arturo Aguilar Sánchez <arturo.aguilar.freelance@gmail.com>
2019-04-19 13:20:27 -0500

    Se agregan los módulos que faltaron en el commit anterior

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './header/header.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+@NgModule({
+    declarations: [
+        BreadcrumbsComponent,
+        HeaderComponent,
+        PageNotFoundComponent,
+        SidebarComponent
+    ],
+
+    exports: [
+        BreadcrumbsComponent,
+        HeaderComponent,
+        PageNotFoundComponent,
+        SidebarComponent
+    ]
+})
+
+export class SharedModule { }
